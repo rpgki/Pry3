974761645c3440d95b5993edbb237c5cb0846df4
rpgki <solfonca@gmail.com>
2016-06-06 00:20:32 -0600

    cambio

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: carlos
+ *
+ * Created on 6 de junio de 2016, 1:26 a.m.
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    cout << "Hello World!" << endl;
+    cout << "Cambio satisfactorio" << endl;
+
+    return 0;
+}
