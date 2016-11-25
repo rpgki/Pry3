d9a46db72da1c48a2af94dd9bd440aef242b4693
Carlos <Carlos@WINDOWS-C2EIL8H>
2016-11-25 01:13:25 -0600

    Se programa la clase Simulador con la adaptacion al mapa y plantilla

--- Pry3/Simulador.cpp
+++ Pry3/Simulador.cpp
@@ -4,21 +4,73 @@
  * 
  * Created on 2 de abril de 2015, 06:46 PM
  */
-
+ 
 #include "Simulador.h"
-
-Simulador::Simulador(Grafo<Nodo, int>& g):grafo(g) {
-    
-}
-
-Simulador::~Simulador() {
-    
-}
-
-void Simulador::iniciarSimulacion(int ios, double vsc, int mvcf, double rc, double grc){
+ 
+Simulador::Simulador(Grafo<int, Nodo>& g):mapAct(g) {
      
+}
+ 
+Simulador::~Simulador() {
+     
+}
+ 
+void Simulador::iniciarSimulacion(int ios, double vsc, int mvcf, double rc, double grc){
+    //Se infectan los vertices de acuerdo a distribucion de probabilidad comparandolo con el parametro ios
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count(); //Se establece la semilla
+    std::default_random_engine generator(seed);
+    std::uniform_int_distribution<int> nmAltr(0, mapAct.obtTotVrt() - 1); //Se genera una distribución uniforme de enteros entre 0 y la cantidad de vertices - 1
+    while (ios != 0) {
+        int num = nmAltr(generator); //Se guarda el número generado
+        if (mapAct[num].obtEst() != 1) {// Se infectan aleatoriamente los nodos de acuerdo al valor ingresado en ios
+            mapAct[num].infectar();
+            ios--;
+        }
+    }
+     
+    //Se aleatoriza el temporizador de chequeo de virus.
+    for(int i = 0; i < mapAct.obtTotVrt(); i++){
+        mapAct[i].azarizarTmpChqVrs(mvcf);
+    }
+     
+    prbInf = vsc; 
+    rec = rc;
+    resis = grc;
  }
-
+ 
 void Simulador::simular() {
-
+    Grafo<int, Nodo> mapAnt(mapAct); //Se crea una instancia de tipo grafo que crea una copia, la cual servira como base para los estados en el tiempo t.
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count(); //Se planta la semilla.
+    std::default_random_engine generator(seed);
+    std::uniform_real_distribution<double> prb(0.0, 1.0); //Se crea el dado.
+    vector<int> vecTemp; //Se crea un vector de adyacentes para cada vertice
+    for (int i = 0; i < mapAnt.obtTotVrt(); i++) { //Se recorre el arreglo de vertices
+        vecTemp = mapAnt.obtAdy(i); //Se guarda el vector de adyacentes.
+        mapAct[i].actCntChqVrs(); //Se actualiza el contador de chequeo de virus
+        //Si el vertice es susceptible, entonces se evalua cada uno de sus adyacentes.
+        //Si alguno infectado se genera un numero aleatorio para evaluar la probabilidad de infeccion.
+        if (mapAnt[i].obtEst() == 0) {
+            for (int j = 0; j < mapAnt.obtTotAdy(i); j++) {
+                if (mapAnt[vecTemp[j]].obtEst() == 1) {
+                    double prbAlt = prb(generator);
+                    if (prbAlt <= prbInf) {
+                        mapAct[i].modEst(Nodo::I);
+                        j = mapAnt.obtTotAdy(i);
+                    }
+                }
+            }
+        } else { //Si el vertice no es susceptible, y esta infectado se evalua la probabilidad de recuperarse.
+            if (mapAnt[i].obtEst() == 1 && mapAnt[i].obtCntChqVrs() == 0) {
+                double prbAlt = prb(generator);
+                if (prbAlt <= rec) {
+                    prbAlt = prb(generator);
+                    if (prbAlt <= resis) { //Si el vertice infectado se recupera, entonces se evalua la probabilidad de que se vuelva resistente al virus.
+                        mapAct[i].modEst(Nodo::R);
+                    } else {
+                        mapAct[i].modEst(Nodo::S);
+                    }
+                }
+            }
+        }
+    }
 }
--- Pry3/Simulador.h
+++ Pry3/Simulador.h
@@ -4,29 +4,30 @@
  *
  * Created on 2 de abril de 2015, 06:46 PM
  */
-
+ 
 #ifndef SIMULADOR_H
-#define	SIMULADOR_H
-
-
+#define SIMULADOR_H
+ 
+ 
 using namespace std;
-
+ 
 #include "Grafo.h"
 #include "Nodo.h"
-
-
+#include <chrono>
+#include <random>
+ 
 class Simulador {
     // Representa el proceso de simulación de la infección en la red de computadoras.
-    
+     
 public:
-    
+     
     // EFE: Construye un simulador que transformará al grafo g.
-    Simulador(Grafo<Nodo, int>& g);
-
+    Simulador(Grafo<int, Nodo>& g);
+ 
     // Destruye a *this retornando toda la memoria asignada dinámicamente.
     ~Simulador();
-
-
+ 
+ 
     // REQ: el grafo asociado (el pasado al constructor) esté bien construido.
     // MOD: el grafo asociado.    
     // EFE: inicializa el simulador con los siguientes parámetros:
@@ -37,7 +38,7 @@ public:
     //      rc o recovery-chance [0..0.1]: probabilidad de recuperación de infección.
     //      grc o gain-resistance-chance [0..1]: probabilidad de lograr resistencia.   
     void iniciarSimulacion(int ios, double vsc, int mvcf, double rc, double grc);
-    
+     
     // REQ: el grafo asociado (el pasado al constructor) esté bien construido.
     // MOD: el grafo asociado.
     // EFE: aplica al grafo asociado una transformación con base en las reglas de 
@@ -49,10 +50,15 @@ public:
     //      3. sólo un vértice recuperado puede ganar resistencia con probabilidad grc.
     //      4. Sólo las transformaciones #2 y #3 pueden ser simultáneas. 
     void simular();
-
+ 
 private:
-    Grafo<Nodo, int>& grafo;
+    Grafo<int, Nodo>& mapAct;
+    int tam; // Cantidad de vertices infectados al azar.
+    double prbInf; // Probabilidad de infeccion
+    int maxFrqChqVrs; // Numero maximo temporizador de chequeo de virus.
+    double rec; // Probabilidad de recuperacion.
+    double resis; // Probabilidad volverse resistente.
     // se deben agregar más variables de estado.
 };
-
-#endif	/* SIMULADOR_H */
+ 
+#endif  /* SIMULADOR_H */
